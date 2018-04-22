477c2d1226b9f313b5ecfa39394f2f8ae80418ee
Andrew E-W <andrewroche@hotmail.com>
2018-04-22 10:28:55 +1000

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DbmlToClass
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInputFile = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxOutputFolder = new System.Windows.Forms.TextBox();
+            this.buttonSelectInput = new System.Windows.Forms.Button();
+            this.buttonSelectOutput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInputFile
+            // 
+            this.textBoxInputFile.Location = new System.Drawing.Point(165, 10);
+            this.textBoxInputFile.Name = "textBoxInputFile";
+            this.textBoxInputFile.Size = new System.Drawing.Size(542, 20);
+            this.textBoxInputFile.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input File Location (.dbml)";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(713, 70);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output Folder Location (.cs)";
+            // 
+            // textBoxOutputFolder
+            // 
+            this.textBoxOutputFolder.Location = new System.Drawing.Point(165, 40);
+            this.textBoxOutputFolder.Name = "textBoxOutputFolder";
+            this.textBoxOutputFolder.Size = new System.Drawing.Size(542, 20);
+            this.textBoxOutputFolder.TabIndex = 4;
+            // 
+            // buttonSelectInput
+            // 
+            this.buttonSelectInput.Location = new System.Drawing.Point(713, 9);
+            this.buttonSelectInput.Name = "buttonSelectInput";
+            this.buttonSelectInput.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelectInput.TabIndex = 5;
+            this.buttonSelectInput.Text = "Select";
+            this.buttonSelectInput.UseVisualStyleBackColor = true;
+            this.buttonSelectInput.Click += new System.EventHandler(this.buttonSelectInput_Click);
+            // 
+            // buttonSelectOutput
+            // 
+            this.buttonSelectOutput.Location = new System.Drawing.Point(713, 38);
+            this.buttonSelectOutput.Name = "buttonSelectOutput";
+            this.buttonSelectOutput.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelectOutput.TabIndex = 6;
+            this.buttonSelectOutput.Text = "Select";
+            this.buttonSelectOutput.UseVisualStyleBackColor = true;
+            this.buttonSelectOutput.Click += new System.EventHandler(this.buttonSelectOutput_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 110);
+            this.Controls.Add(this.buttonSelectOutput);
+            this.Controls.Add(this.buttonSelectInput);
+            this.Controls.Add(this.textBoxOutputFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxInputFile);
+            this.Name = "FormMain";
+            this.Text = "DBML to Class";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInputFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxOutputFolder;
+        private System.Windows.Forms.Button buttonSelectInput;
+        private System.Windows.Forms.Button buttonSelectOutput;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace DbmlToClass
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            textBoxInputFile.Text = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "input", "sample.dbml");
+            textBoxOutputFolder.Text = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "output");
+        }
+
+        private void buttonSelectInput_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Path.GetDirectoryName(textBoxInputFile.Text);
+            openFileDialog1.FileName = Path.GetFileName(textBoxInputFile.Text);
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                textBoxInputFile.Text = openFileDialog1.FileName;
+        }
+
+        private void buttonSelectOutput_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = textBoxOutputFolder.Text;
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                textBoxOutputFolder.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBoxInputFile.Text))
+            {
+                MessageBox.Show("The specified input file could not be located.");
+                return;
+            }
+
+            if (!Directory.Exists(textBoxOutputFolder.Text))
+            {
+                if (MessageBox.Show("The specified output folder could not be located. Do you want to create it?") == DialogResult.Yes)
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(textBoxOutputFolder.Text);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("The folder could not be created, please check the path or use an existing folder.");
+                        return;
+                    }
+                }
+                else
+                    return;
+            }
+
+            //read and parse the XML data
+            string xml = File.ReadAllText(textBoxInputFile.Text);
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xml);
+
+            XmlNode databaseNode = null;
+
+            foreach (XmlNode xmlNode in xmlDocument.ChildNodes)
+                if (xmlNode.Name == "Database")
+                    databaseNode = xmlNode;
+
+            foreach (XmlNode tableNode in databaseNode.ChildNodes)
+            {
+                if (tableNode.Name == "Table")
+                {
+                    XmlNode typeNode = tableNode.FirstChild;
+
+                    if (typeNode != null && typeNode.Name == "Type")
+                    {
+                        string typeName = typeNode.Attributes["Name"].Value;
+
+                        string classText = string.Empty;
+
+                        classText += "using System;" + "\r\n";
+                        classText += "" + "\r\n";
+                        classText += "namespace TemporaryNamespace" + "\r\n";
+                        classText += "{" + "\r\n";
+                        classText += "    public class " + typeName + "\r\n";
+                        classText += "    {" + "\r\n";
+
+                        foreach (XmlNode columnNode in typeNode.ChildNodes)
+                        {
+                            if (columnNode.Name == "Column")
+                            {
+                                Type type = Type.GetType(columnNode.Attributes["Type"].Value);
+                                bool isNullable = bool.Parse(columnNode.Attributes["CanBeNull"].Value) && type.IsValueType;
+                                string name = columnNode.Attributes["Name"].Value;
+
+                                classText += "        public " + type.Name + (isNullable ? "?" : "") + " " + name + " { get; set; }" + "\r\n";
+                            }
+                        }
+
+                        classText += "    }" + "\r\n";
+                        classText += "}" + "\r\n";
+
+                        string outputFile = Path.Combine(textBoxOutputFolder.Text, typeName + ".cs");
+                        File.WriteAllText(outputFile, classText);
+                    }
+                }
+            }
+
+            MessageBox.Show("Done!");
+        }
+    }
+}
